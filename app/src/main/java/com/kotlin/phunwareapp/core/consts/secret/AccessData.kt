5f685114809ba3dc3808f5cbc3b96cdc6bdbfbf4
RealformStudio <sl4sh8@gmail.com>
2024-10-29 23:50:13 -0600

    Lets prepare the Objects necesary to hold the information of the endpoints this is not the ultimate recommendation because usually this endpoints can be asked so they are not store in the app in any class... in the past this sort of information was stored in build.gradle local.properties XML files or JSON files
    in this case for showinf purposes it was store in an object
    
    also as a singleton it is accessible from anywhere just using the name of the Object

---
+++ app/src/main/java/com/kotlin/phunwareapp/core/consts/secret/AccessData.kt
@@ -0,0 +1,6 @@
+package com.kotlin.phunwareapp.core.consts.secret
+
+object AccessData {
+    const val BASE_URL = "https://raw.githubusercontent.com/phunware-services/dev-interview-homework/master/"
+    const val IMG_BASE_URL="https://raw.githubusercontent.com/phunware-services/dev-interview-homework/master/Images/"
+}
